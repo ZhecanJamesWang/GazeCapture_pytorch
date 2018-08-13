276b796ead0bb34137a0e3808e325fc63ce8175f
zhecan wang <zhecanwang@zhecans-MacBook-Pro.local>
2018-08-13 23:03:56 +0800

    update

--- process_data.py
+++ process_data.py
@@ -33,7 +33,7 @@ def load_batch_from_data(names, path, batch_size, img_ch, img_cols, img_rows, tr
 		counter += 1
 		# directory
 		dir = img_name[:5]
-		if int(dir) >= 153:
+		if int(dir) >= 475:
 			# frame name
 			frame = img_name[6:]
 
@@ -96,24 +96,24 @@ def load_batch_from_data(names, path, batch_size, img_ch, img_cols, img_rows, tr
 			br_y = tl_y + h
 			right_eye = img[tl_y:br_y, tl_x:br_x]
 
-			# get face grid (in ch, cols, rows convention)
-			# face_grid = np.zeros(shape=(25, 25, 1), dtype=np.float32)
-			face_grid = np.zeros(shape=(25, 25), dtype=np.float32)
-			tl_x = int(grid_json["X"][idx])
-			tl_y = int(grid_json["Y"][idx])
-			w = int(grid_json["W"][idx])
-			h = int(grid_json["H"][idx])
-			br_x = tl_x + w
-			br_y = tl_y + h
+			# # get face grid (in ch, cols, rows convention)
+			# # face_grid = np.zeros(shape=(25, 25, 1), dtype=np.float32)
+			# face_grid = np.zeros(shape=(25, 25), dtype=np.float32)
+			# tl_x = int(grid_json["X"][idx])
+			# tl_y = int(grid_json["Y"][idx])
+			# w = int(grid_json["W"][idx])
+			# h = int(grid_json["H"][idx])
+			# br_x = tl_x + w
+			# br_y = tl_y + h
+			#
+			# # print ("face_grid: ", face_grid.shape)
+			# # face_grid[0, tl_y:br_y, tl_x:br_x] = 1
+			# face_grid[tl_y:br_y, tl_x:br_x] = 1
+			# # face_grid = face_grid.flatten()
 
-			# print ("face_grid: ", face_grid.shape)
-			# face_grid[0, tl_y:br_y, tl_x:br_x] = 1
-			face_grid[tl_y:br_y, tl_x:br_x] = 1
-			# face_grid = face_grid.flatten()
-
-			# get labels
-			y_x = dot_json["XCam"][idx]
-			y_y = dot_json["YCam"][idx]
+			# # get labels
+			# y_x = dot_json["XCam"][idx]
+			# y_y = dot_json["YCam"][idx]
 
 			for folder in ["/appleFace/", "/appleLeftEye/", "/appleRightEye/"]:
 				p = join(path, dir) + folder
